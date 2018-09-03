06ed4013de7f38adfae5993bc3e54579281e109f
fadikamohamed <fadika.mohamed02@gmail.com>
2018-09-03 14:49:01 +0200

    exercice terminé

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+  $isOk = false;
+?>
+<!DOCTYPE html>
+<html lang="fr">
+  <head>
+    <meta charset="utf-8" />
+    <title>Exerceice 7</title>
+  </head>
+  <body>
+    <p>
+    <?php
+      if ($isOk == false) {
+        echo 'C\'est pas bon !!!';
+      } else {
+        echo 'C\'est ok !!';
+      }
+     ?>
+   </p>
+  </body>
+</html>
